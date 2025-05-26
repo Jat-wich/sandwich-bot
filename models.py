5798949d7541bfb43d9bbdd02f693cde0b017939
Jat-wich <jatinder13012001@gmail.com>
2025-05-26 21:48:32 +0200

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-DATABASE_URL = os.getenv("postgresql://sandwichbot_db_user:vQv9neKVNkQ3xaC2FfemcGamfrNLeIWs@dpg-d0qbslgdl3ps73equv3g-a/sandwichbot_db")
+DATABASE_URL = os.getenv("postgres://sandwichbot_db_user:vQv9neKVNkQ3xaC2FfemcGamfrNLeIWs@dpg-d0qbslgdl3ps73equv3g-a/sandwichbot_db")
 
 engine = create_engine(DATABASE_URL, echo=False)
 Base = declarative_base()

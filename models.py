11c068596d3899e0ffa450cbfe82ac66990a0cc1
Jat-wich <jatinder13012001@gmail.com>
2025-05-27 19:05:46 +0200

    added env file

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 # models.py
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("postgres://sandwichbot_db_user:vQv9neKVNkQ3xaC2FfemcGamfrNLeIWs@dpg-d0qbslgdl3ps73equv3g-a/sandwichbot_db")
+
 
 engine = create_engine(DATABASE_URL, echo=False)
 Base = declarative_base()
